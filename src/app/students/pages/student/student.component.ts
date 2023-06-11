1da80694e830b177065e4389c4031d5ec4d77a08
Bladimir Gonzales <bladimirg@gmail.com>
2023-06-11 22:47:19 +0200

    create student and auth module

---
+++ src/app/students/pages/student/student.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student',
+  templateUrl: './student.component.html',
+  styles: [
+  ]
+})
+export class StudentComponent {
+
+}
---
+++ src/app/students/students.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StudentComponent } from './pages/student/student.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ListComponent } from './pages/list/list.component';
+import { SearchComponent } from './pages/search/search.component';
+import { AddComponent } from './pages/add/add.component';
+
+
+
+@NgModule({
+  declarations: [
+    StudentComponent,
+    HomeComponent,
+    ListComponent,
+    SearchComponent,
+    AddComponent
+  ],
+  exports:[
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class StudentsModule { }
