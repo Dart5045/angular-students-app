0fc01fd863ee1d8a0036808a776cf4b730a145f7
Bladimir Gonzales <bladimirg@gmail.com>
2023-06-11 23:28:11 +0200

    add root routing
    Also, create an error page component

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes ,RouterModule  } from '@angular/router';
+import { ErrorPageComponent } from './shared/error-page/error-page.component';
+
+const routes:Routes  = [
+  {
+    path:'404',
+    component: ErrorPageComponent,
+
+  },
+  {
+    path:'**',
+    //component: ErrorPageComponent,
+    redirectTo: '404'
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports:[
+    RouterModule
+  ]
+})
+export class AppRoutingModule { 
+
+}
